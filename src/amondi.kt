9be3351ef0d068036d4d8c6af3cdf099148d60e4
loycevelma <amondiloyce@gmail.com>
2021-03-03 09:42:07 +0300

    second assignment

---
+++ src/amondi.kt
@@ -0,0 +1,33 @@
+fun main() {
+ name()
+    modulus()
+    add()
+value()
+
+
+}
+fun name () {
+    println("hello marie")
+
+}
+fun modulus() {
+    var num1=45
+    var num2=40
+    var module= num1%num2
+    println(module)
+}
+fun add(){
+    var num1=20
+    var num2=10
+    var num3=34
+    var num4=4
+    var add=num1+num2+num3+num4
+    println(add)
+}
+fun value(){
+    println("honest")
+}
+
+
+
+
